acef3fbe356bfb49bd8a5227d8a46f8c5ec2ace6
Victor Glez <77651273+Xynthmind@users.noreply.github.com>
2023-06-01 20:04:45 -0600

    Encrip finish

--- src/views/ChallengeONE.js
+++ src/views/ChallengeONE.js
@@ -5,11 +5,47 @@ import Form from 'react-bootstrap/Form';
 
 export default function ChallengeONE() {
     var textToE = "";
-    const outTxt = "";
-    const handleChange = (event) => {
+    var outTxt = "";
+
+    const handleChange = (event) => { //recibimos los cambios del área de texto y los asignamos a la variable textToE
         const { value } = event.target;
         textToE = value;
-        console.log(textToE);
+        //console.log(textToE);
+    }
+
+    function encrip() {
+        var textarray = textToE.split(" "); //se efectua un array de palabras
+        var outTxt = "";
+
+        for (var i = 0; i < textarray.length; i++) { //se hace un for de 0 hasta la cantidad de palabras
+            var tempword = textarray[i]; //tempword va a almacenar la palabra que se encuentra en el indice i
+            for (var j = 0; j < tempword.length; j++) {
+                var arraytemp = tempword.split(''); //a un array temporal le pondremos la palabra dividia por letras
+                if (arraytemp[j] == "a") {
+                    arraytemp[j] = "ai";
+                } else {
+                    if (arraytemp[j] == "e") {
+                        arraytemp[j] = "enter";
+                    } else {
+                        if (arraytemp[j] == "i") {
+                            arraytemp[j] = "imes";
+                        } else {
+                            if (arraytemp[j] == "o") {
+                                arraytemp[j] = "ober";
+                            } else {
+                                if (arraytemp[j] == "u") {
+                                    arraytemp[j] = "ufat";
+                                }
+                            }
+                        }
+                    }
+                } //end if else   
+                
+                outTxt = outTxt + arraytemp[j];
+            } //end for 2
+            outTxt = outTxt + " ";
+        }
+        console.log(outTxt);
     }
     return (
         <div className={styles.bcgradient} >
@@ -18,16 +54,22 @@ export default function ChallengeONE() {
             </div>
             <div className={styles.txtArea}>
                 <label>
-                    <textarea rows={6} cols= {56} style={{width: "100%", height: "59vh"}} onChange={handleChange}/>
+                    <textarea rows={6} cols={56}
+                        style={{ width: "155%" }}
+                        onChange={handleChange} />
                 </label>
             </div>
             <div className={styles.txtResult}>
-
+                <label>
+                    <textarea rows={6} cols={56}
+                        style={{ width: "155%", height: "60vh" }}
+                        onChange={handleChange} />
+                </label>
             </div>
             <div className={styles.footerP}>
                 <Form>
                     <Form.Group>
-                        <Button variant="success">
+                        <Button variant="success" onClick={encrip}>
                             Encriptar
                         </Button>
                         <Button variant="danger">
